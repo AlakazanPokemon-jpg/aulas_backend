8afed29d2bf86471d77f179c0e023805395aa44a
AlakazanPokemon-jpg <guilhermegmadalena@gmail.com>
2025-08-07 20:00:40 -0300

    ASIDIWAD

--- Saudacao.java
+++ Saudacao.java
@@ -1,24 +1,36 @@
-import java.util.Scanner;
-
 public class Saudacao {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        String nome = "Guilherme";
+        int idade = 19;
 
-        System.out.print("Digite seu nome: ");
-        String nome = scanner.nextLine();
+        System.out.println(nome + " , " + idade + " anos");
 
-        System.out.print("Digite sua idade: ");
-        int idade = scanner.nextInt();
-
-        System.out.println("Olá, " + nome + "!");
-
-        if (idade >= 18) {
-            System.out.println("Você é maior de idade.");
-        } else {
-            System.out.println("Você é menor de idade.");
-        }
-
-        scanner.close();
     }
 }
+
+
+// import java.util.Scanner;
+
+// public class Saudacao {
+
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+
+//         System.out.print("Digite seu nome: ");
+//         String nome = scanner.nextLine();
+
+//         System.out.print("Digite sua idade: ");
+//         int idade = scanner.nextInt();
+
+//         System.out.println("Olá, " + nome + "!");
+
+//         if (idade >= 18) {
+//             System.out.println("Você é maior de idade.");
+//         } else {
+//             System.out.println("Você é menor de idade.");
+//         }
+
+//         scanner.close();
+//     }
+// }
