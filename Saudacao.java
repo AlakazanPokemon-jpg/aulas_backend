4fc7b3b376a9a2f6ec78f485d92a8a8c882c1e04
AlakazanPokemon-jpg <guilhermegmadalena@gmail.com>
2025-08-07 19:37:41 -0300

    Versao Final

--- Saudacao.java
+++ Saudacao.java
@@ -5,18 +5,14 @@ public class Saudacao {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Lê o nome
         System.out.print("Digite seu nome: ");
         String nome = scanner.nextLine();
 
-        // Lê a idade
         System.out.print("Digite sua idade: ");
         int idade = scanner.nextInt();
 
-        // Mostra saudação
         System.out.println("Olá, " + nome + "!");
 
-        // Verifica se é maior de idade
         if (idade >= 18) {
             System.out.println("Você é maior de idade.");
         } else {
